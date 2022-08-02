3f12e89cb7f8a32eb076b23d0732648d018d84d8
firas-ibraheem <ferasibraheem2@gmail.com>
2022-08-02 23:51:16 +0300

    change

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var Name, Age, gender, Skip;
 alert("Please enter all the required spaces");
+var Name, Age, gender, Skip, Answer,
 Name = prompt("Give me your  Name");
 Age = Number(prompt("Give me your Age"));
 gender = prompt("Give me your Gender");
-Skip = confirm("Skip welcoming Message");
+Skip = prompt("DO you wante skip welcoming Message"),confirm("Skip welcoming Message");
+
 
 function age() {
   if (Age <= 0 ){
@@ -16,7 +17,7 @@ function Gender() {
     return prompt("Hello Ms " + Name);
     }
   else if (gender === "male"){
-    return prompt("Hello Mr " + Name);
+    return alert("Hello Mr " + Name);
     
     }
     else{
@@ -24,14 +25,29 @@ function Gender() {
 
     }
 }
-var myarr = []; 
- 
+var myArr = [2];
 function addTo() { 
-    for(i = 0; i <= 2;i++){
-   myarr.push(document.getElementById(i)); 
-   }
-   console.log(myarr);
+    
+    var size = 2; // Array size
+    var questionArray = [2]
+    questionArray[0] = "Do you like python";
+    questionArray[1] = "Do you like html";
+    questionArray[2] = "Do you like java";
+    for(let i=0; i<=size; i++) 
+    {
+        myArr[i] = prompt( questionArray[i]);
+        if (typeof prompt != "yes"||"no"){
+            alert("in valid");
+        }
+
+    }
+   
 } 
 addTo();
-  age();
-Gender();
+console.log(myArr);
+ // age();
+//Gender();
+
+
+
+
